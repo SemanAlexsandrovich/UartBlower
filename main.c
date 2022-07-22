bd9b8341599ffc3247378069c8fa0f65c4b25d55
seman <sema.shmykov@mail.ru>
2022-07-22 14:06:16 +0300

    This is the final version of the project without the LCD display

--- main.c
+++ main.c
@@ -4,13 +4,12 @@
  * Author : sshmykov
  */ 
  /*
-In this branch I am trying to make a rotation counter.
-Add new Timer in pwm.c
+this is the final version of the project without the LCD display
  */
 #define F_CPU 16000000L
+#define TICK_PWM 23//(1/F_PWM) = 1/43360 = 23/1 000 000
 #include <avr/io.h>
 #include <avr/interrupt.h>
-//#include <avr/sleep.h>
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
@@ -26,11 +25,17 @@ volatile uint8_t pos = 0;
 volatile uint8_t j = 0;
 volatile uint8_t flag = 0;
 volatile uint8_t pwm = 0;
-//volatile uint16_t number_of_pulses = 0;
-//volatile uint32_t rev_per_min = 0;
+volatile uint16_t time_fixation = 0;
 volatile uint8_t flag_rev = 0;
 volatile uint16_t analys_increment = 0;
-uint32_t time_between_ticks[DEPTH_OF_ANALYSIS] = {0};
+volatile uint8_t pulse_capture_flag = 0;
+volatile uint32_t max_ticks_between_pulses = 0;
+volatile uint32_t min_ticks_between_pulses = 65535;
+volatile uint16_t rev_per_min = 0;
+volatile uint16_t rev_per_sec = 0;
+volatile uint32_t mid_ticks_between_pulses;
+volatile uint32_t real_time_between_pulses;
+volatile uint32_t real_rev_time;
 
 const char command_on[] = "On\r";
 const char command_off[] = "Off\r";
@@ -102,36 +107,30 @@ int main(void) {
 			UCSR0B |= (1 << UDRIE0);
 			flag_recive = 0;
 		} 
-		if (flag_rev) {
-			uint32_t max_time = time_between_ticks[0];
-			uint32_t min_time = time_between_ticks[0];
-			for (analys_increment = 0; analys_increment  < DEPTH_OF_ANALYSIS; analys_increment++){
-				if(time_between_ticks[analys_increment] > max_time) {
-					max_time = time_between_ticks[analys_increment];
-				}
-				if(time_between_ticks[analys_increment] <  min_time) {
-						min_time = time_between_ticks[analys_increment];
-				}
+		if (pulse_capture_flag) {
+			if (time_fixation > max_ticks_between_pulses) {
+				max_ticks_between_pulses = time_fixation;
 			}
-			uint32_t mid_time_per_rev = (max_time + min_time);
-			/*
-			in one revolution we have two pulses, so
-			mid_time = (max_time + min_time) /2;
-			mid_time_per_rev = mid_time * 2;// or mid_time_per_rev = ((max_time + min_time)/2 )* 2 ;
-			*/
-			//1 tick timmer1 = 1 tick CPU. 1tick -> 1/F_CPU
-			long double real_time = ((long double)mid_time_per_rev / F_CPU);//one rotation time = ticks_between_pulses * one_tick_CPU_time
-			uint16_t rev_per_sec = 1 /real_time;//RPS = 1sec / one rotation time
-			uint32_t rev_per_min = rev_per_sec * 60;//RPM = RPS * 60
-			
-
-
-
-
-			sprintf((char*)buff_to_send, "rev: %lu RPM\r\n", rev_per_min);
+			if (time_fixation < min_ticks_between_pulses) {
+				min_ticks_between_pulses = time_fixation;
+			}
+			pulse_capture_flag = 0;
+		}
+		if (flag_rev) { 
+			if (max_ticks_between_pulses >= min_ticks_between_pulses) {
+				mid_ticks_between_pulses = ((max_ticks_between_pulses + min_ticks_between_pulses) >> 1);
+				real_time_between_pulses = (mid_ticks_between_pulses * TICK_PWM);//real_time_between_pulses * 1 000 000
+				real_rev_time = 2 * real_time_between_pulses;//real_rev_time (* 1000
+				rev_per_sec = 1000000/real_rev_time;//RPS = 1sec/real_rev_time		
+			} else {
+				rev_per_sec = 0;
+			}
+			rev_per_min = rev_per_sec * 60;//RPM = RPS * 60
+			sprintf((char*)buff_to_send, "rev: %d RPM\r\n", rev_per_min);
 			DebagUart(buff_to_send);
 			UCSR0B |= (1 << UDRIE0);
-			memset(time_between_ticks, 0, 10);
+			max_ticks_between_pulses = 0;
+			min_ticks_between_pulses = 65535;
 			flag_rev = 0;
 		}
 	}
--- pwm.c
+++ pwm.c
@@ -12,98 +12,63 @@
 
 #include "pwm.h"
 
-//#define PWM_FREQ_HZ 25000L
 #define PWM_PIN 6
 #define COUNT_PIN 3
 #define TOP 255
 #define ONE_SEC 1000
 
 volatile extern uint8_t flag_rev;
-//volatile extern uint16_t number_of_pulses;
-volatile extern uint32_t rev_per_min;
-volatile extern uint16_t analys_increment;
-extern uint32_t time_between_ticks[DEPTH_OF_ANALYSIS];
+volatile extern uint16_t time_fixation;
+volatile extern uint8_t pulse_capture_flag;
+volatile uint8_t interrupt_flag = 0;
+volatile uint16_t tick_counter = 0;
 
-volatile uint8_t point_flag = 1;
-volatile uint8_t first_interrrupt_flag = 1;
-volatile uint16_t starting_point = 0;
-volatile uint16_t end_of_point = 0;
-
-ISR(TIMER1_COMPA_vect) {//timer interrupt once per second
+ISR(TIMER1_COMPA_vect) {//couunt ticks from pulse to pulse
 	cli();
-	/*
-		приведение времени между импульсами к оборотам 
-		сделаю в основном цикле,чтобы не нагружать прерывание.
-		возможно нужно перенести всю эту обработку в основной цикл а сдесь лишь поднять флаг 
-	*/
-
-	flag_rev = 1;
-
+	if (interrupt_flag) {
+		tick_counter++;		
+	}
+	//couunt one second
+	static uint16_t count_to_the_second = 0;
+	count_to_the_second++;
+	if (count_to_the_second == 43360) {
+		count_to_the_second = 0;
+		flag_rev = 1;
+	}
 	sei();
 }
 
-ISR(INT1_vect) {
+ISR(INT1_vect) {//pulse_capture
 	cli();
-	//the first interruption must be skipped
-	if (first_interrrupt_flag) {
-		if (point_flag) {//end previos impulse
-			starting_point = TCNT1;
-		} else {//begin new impulse
-			end_of_point = TCNT1;
-			/*
-			after obtaining the necessary information 
-			(end of the previous pulse, start of a new pulse) 
-			we will immediately analyze.
-			*/
-			if (end_of_point < starting_point) {
-				end_of_point = (uint32_t)end_of_point + 65536;
-			}
-			if (analys_increment  < DEPTH_OF_ANALYSIS){
-				time_between_ticks[analys_increment] = end_of_point - (uint32_t)starting_point;
-				analys_increment++;
-			}
-			//нужно ли приводить типы обратно?
-			starting_point = (uint16_t)starting_point;
-			end_of_point = (uint16_t)end_of_point;
-			/*
-			необходимо сделать анализ time_between_ticks:
-			выбрать максимальное и минимальное
-			чтобы не засорять это прерывание, сделаю анализ в прерывании по секунде.
-			А здесь сохраню в массив лишь 10 значений time_between_ticks.
-			так как у нас происходит около 1000 импульсов в секуду, 
-			то для большей точности нужен массив из 1000 
-			элементов с time_between_ticks, но это очень затратно
-			*/
+	interrupt_flag = !interrupt_flag;
+	if (interrupt_flag == 0) {
+		if (pulse_capture_flag == 0) {
+			time_fixation = tick_counter;
+			tick_counter = 0;
+			pulse_capture_flag = 1;
 		}
-		point_flag = !point_flag;
+		interrupt_flag = 1;
 	}
-	first_interrrupt_flag = 1;
 	sei();
 }
 
 void timer_init(void) {
-  	// 1 Hz (16000000/((15624+1)*1024))
-	OCR1A = 15624;
-	// CTC
+	// 43360.433604336045 Hz (16000000/((368+1)*1))
+	//this frequency is sufficient to ensure the accuracy of the RPM
+	OCR1A = 368;
+ 	// CTC
 	TCCR1B |= (1 << WGM12);
-	// Prescaler 1024
-	TCCR1B |= (1 << CS12) | (1 << CS10);
-	// Output Compare Match A Interrupt Enable
+	// Prescaler 1
+	TCCR1B |= (1 << CS10);
+	//Output Compare Match A Interrupt Enable
 	TIMSK1 |= (1 << OCIE1A);
-	TCNT1 = 0; 
-	memset(time_between_ticks, 0, 10);
 }
 
 void counter_init(void) {
-	DDRD &= ~(1 << COUNT_PIN);//Pin 3 of the arduino uno(PD2)
+	DDRD &= ~(1 << COUNT_PIN);//INT1 Pin 3 of the arduino uno(PD3)
 	PORTD |= (1 << COUNT_PIN); 
 
 	EIMSK = (1 << INT1);
-	/*
-	Any logical change on INT0 generates an interrupt request.
-	does not fit because for one tachometer signal 
-	we will increase the counter by 2, not by 1
-	*/
 	//The rising edge of INT1 generates an interrupt request.
 	EICRA |= (1 << ISC10);
 	EICRA |= (1 << ISC11);
@@ -111,7 +76,7 @@ void counter_init(void) {
 
 void pwm_init(void) {
 
-	DDRD |= (1 << PWM_PIN);//Pin 9 of the arduino uno (PB1)
+	DDRD |= (1 << PWM_PIN);//Pin 6 of the arduino uno (PD6)
 
 	
 	TCCR0A |= (1 << WGM00) | (1 << COM0A1);
